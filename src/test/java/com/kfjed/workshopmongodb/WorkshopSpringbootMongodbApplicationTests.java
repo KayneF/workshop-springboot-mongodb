4f7b29fe85d1e5663ed6279536639cd42faceeea
KayneFerreira <kayne.ferreira@fatec.sp.gov.br>
2022-11-09 13:39:08 -0300

    Project created

---
+++ src/test/java/com/kfjed/workshopmongodb/WorkshopSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfjed.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
