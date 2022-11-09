41cfabd0623eefa39ed8d7fd2e484a8e2b3462de
KayneFerreira <kayne.ferreira@fatec.sp.gov.br>
2022-11-09 17:07:12 -0300

    Get user by id; Resources exception handlers

--- src/main/java/com/kfjed/workshopmongodb/services/UserService.java
+++ src/main/java/com/kfjed/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.kfjed.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kfjed.workshopmongodb.domain.User;
 import com.kfjed.workshopmongodb.repository.UserRepository;
+import com.kfjed.workshopmongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
