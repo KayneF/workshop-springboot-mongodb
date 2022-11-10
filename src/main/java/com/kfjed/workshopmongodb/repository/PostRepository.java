32e5ae33619ea664b8a6dc11e79647044e25c20f
KayneFerreira <kayne.ferreira@fatec.sp.gov.br>
2022-11-09 21:39:46 -0300

    Simple query method with @Query

--- src/main/java/com/kfjed/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/kfjed/workshopmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.kfjed.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.kfjed.workshopmongodb.domain.Post;
@@ -10,5 +11,8 @@ import com.kfjed.workshopmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
