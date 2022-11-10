d6de2490826e3254d0b0ca27e429dfd7bbb4064e
KayneFerreira <kayne.ferreira@fatec.sp.gov.br>
2022-11-09 22:50:24 -0300

    Full search criteria method added

--- src/main/java/com/kfjed/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/kfjed/workshopmongodb/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.kfjed.workshopmongodb.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -14,5 +15,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
 	
-	List<Post> findByTitleContainingIgnoreCase(String text);
+	List<Post> findByTitleContainingIgnoreCase(String text);	//not used
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
